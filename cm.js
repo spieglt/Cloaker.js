d05164e50ae09751840142fce80adb77771867f0
Theron <tspiegl@gmail.com>
2021-03-30 20:03:39 -0500

    tweaks, css

--- cm.js
+++ cm.js
@@ -29,7 +29,10 @@ window.onload = () => {
   encryptElem.oninput = async (e) => {
     const password = passwordBox.value;
     if (password.length < 12) {
-      alert('Password must be at least 12 characters. 16 or more is recommended.');
+      passwordBox.classList.add('passwordError');
+      setTimeout(() => {
+        passwordBox.classList.remove('passwordError');
+      }, 1000);
       return;
     }
     startEncryption(encryptElem.files[0], password);
@@ -46,7 +49,7 @@ window.onload = () => {
 };
 
 
-let filename, inBuffer, outBuffers, outFile, downloadLink;
+let filename, inBuffer, outBuffers, outputFilename, outFile, downloadLink;
 let worker = new Worker('./worker.js');
 
 worker.onmessage = (message) => {
@@ -58,33 +61,35 @@ worker.onmessage = (message) => {
       break;
     case 'encryptedChunk':
       outBuffers.push(message.data.encryptedChunk);
-      replace(message.data.progress);
+      updateProgress(message.data.progress);
       worker.postMessage({ command: 'encryptChunk' }); // next chunk
       break;
     case 'finalEncryption':
       outBuffers.push(message.data.encryptedChunk);
-      replace(message.data.progress);
-      outFile = new File(outBuffers, filename + '.cloaker');
-      output('\ndone\n');
+      updateProgress(message.data.progress);
+      outputFilename = filename + '.cloaker';
+      outFile = new File(outBuffers, outputFilename);
+      output('done');
       downloadLink = document.getElementById('downloadLink');
       downloadLink.download = filename + '.cloaker';
-      downloadLink.style = '';
       downloadLink.href = URL.createObjectURL(outFile);
+      downloadLink.innerText = `Download encrypted file "${outputFilename}"`
+      downloadLink.style = 'display: unset';
       break;
     case 'initializedDecryption':
       worker.postMessage({ command: 'decryptChunk' }); // kick off decryption
       break;
     case 'decryptedChunk':
       outBuffers.push(message.data.decryptedChunk);
-      replace(message.data.progress);
+      updateProgress(message.data.progress);
       worker.postMessage({ command: 'decryptChunk' });
       break;
     case 'finalDecryption':
       outBuffers.push(message.data.decryptedChunk);
-      replace(message.data.progress);
+      updateProgress(message.data.progress);
       // if filename is longer than .cloaker and ends with .cloaker, chop off extension. if not, leave as is and let the user or OS decide.
       let suffixes = ['.cloaker', '.cloaker.txt']; // Chrome on Android adds .cloaker.txt for some reason
-      let outputFilename = filename;
+      outputFilename = filename;
       for (i in suffixes) {
         let len = suffixes[i].length;
         if (filename.length > len && filename.slice(filename.length - len, filename.length) === suffixes[i]) {
@@ -92,17 +97,18 @@ worker.onmessage = (message) => {
         }
       }
       outFile = new File(outBuffers, outputFilename);
-      output('\ndone\n');
+      output('done');
       downloadLink = document.getElementById('downloadLink');
       downloadLink.download = outputFilename;
-      downloadLink.style = '';
       downloadLink.href = URL.createObjectURL(outFile);
+      downloadLink.innerText = `Download decrypted file "${outputFilename}"`
+      downloadLink.style = 'display: unset';
       break;
     case 'decryptionFailed':
-      output('incorrect password\n');
+      output('incorrect password');
       break;
     case 'notCloaker':
-      output('file was not encrypted with cloaker\n');
+      output('file was not encrypted with cloaker');
       break;
   }
 };
@@ -111,7 +117,7 @@ startEncryption = async (inFile, password) => {
   inBuffer = await inFile.arrayBuffer();
   outBuffers = [new Uint8Array(SIGNATURE)];
   filename = inFile.name;
-  output(`filename: ${filename}, size: ${inBuffer.byteLength}\n`);
+  output(`filename: ${filename}, size: ${inBuffer.byteLength}`);
   let salt = new Uint8Array(crypto_pwhash_argon2id_SALTBYTES);
   window.crypto.getRandomValues(salt);
   outBuffers.push(salt);
@@ -122,17 +128,26 @@ startDecryption = async (inFile, password) => {
   inBuffer = await inFile.arrayBuffer();
   outBuffers = [];
   filename = inFile.name;
-  output(`filename: ${filename}, size: ${inBuffer.byteLength}\n`);
+  output(`filename: ${filename}, size: ${inBuffer.byteLength}`);
   worker.postMessage({ inBuffer, password, filename, command: 'startDecryption' }, [inBuffer]); // make sure to transfer inBuffer, not clone
 }
 
 const output = (msg) => {
+  if (window.getComputedStyle(outputBox).display === 'none') {
+    outputBox.style = 'display: unset';
+  }
   let message = document.createElement('span');
   message.textContent = msg;
   outputBox.appendChild(message);
   outputBox.appendChild(document.createElement('br'));
 }
 
-const replace = (msg) => {
+const updateProgress = (msg) => {
+  if (window.getComputedStyle(progressBar).display === 'none') {
+    progressBar.style = 'display: unset';
+  }
+  if (!(typeof msg === 'number')) {
+    msg = 100; // if empty file, we divided by 0 in worker, so msg will be NaN
+  }
   progressBar.value = msg;
 }
