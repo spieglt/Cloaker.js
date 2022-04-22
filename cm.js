de45fec1633e04b806071e00351232d031a6fe08
Theron <tspiegl@gmail.com>
2022-04-22 16:26:00 -0500

    Revert "Merge pull request #1 from spieglt/streaming"
    This reverts commit fbb01603212c82427376b11a1f18dc69828fa531, reversing
    changes made to 66aadf555334bb108eeaae318a4866ca4d30a95c.

---
+++ cm.js
@@ -0,0 +1,153 @@
+const crypto_pwhash_argon2id_SALTBYTES = 16;
+const SIGNATURE = new Uint8Array([0xC1, 0x0A, 0x6B, 0xED]);
+
+let startEncryption;
+let encryptChunk;
+let startDecryption;
+let decryptChunk;
+let encryptButton;
+let encryptElem;
+let decryptButton;
+let decryptElem;
+let passwordTitle;
+let passwordBox;
+let outputBox;
+let progressBar;
+
+window.onload = () => {
+  encryptButton = document.getElementById('encryptButton');
+  encryptElem = document.getElementById('encryptElem');
+  decryptButton = document.getElementById('decryptButton');
+  decryptElem = document.getElementById('decryptElem');
+  passwordTitle = document.getElementById('passwordTitle');
+  passwordBox = document.getElementById('passwordBox');
+  outputBox = document.getElementById('outputBox');
+  progressBar = document.getElementById('progressBar');
+
+  encryptButton.onclick = () => encryptElem.click();
+
+  encryptElem.oninput = () => {
+    const password = passwordBox.value;
+    if (password.length < 12) {
+      passwordBox.classList.add('passwordError');
+      setTimeout(() => {
+        passwordBox.classList.remove('passwordError');
+      }, 1000);
+      passwordTitle.classList.add('passwordErrorTitle');
+      setTimeout(() => {
+        passwordTitle.classList.remove('passwordErrorTitle');
+      }, 4000);
+      return;
+    }
+    startEncryption(encryptElem.files[0], password);
+  };
+
+  decryptButton.onclick = () => decryptElem.click();
+
+  decryptElem.oninput = () => {
+    const password = passwordBox.value;
+    startDecryption(decryptElem.files[0], password);
+  };
+};
+
+
+let filename, inBuffer, outBuffers, outputFilename, outFile, downloadLink;
+let worker = new Worker('./worker.js');
+
+worker.onmessage = (message) => {
+  // console.log('main received:', message);
+  switch (message.data.response) {
+    case 'initializedEncryption':
+      outBuffers.push(message.data.header);
+      worker.postMessage({ command: 'encryptChunk' }); // kick off actual encryption
+      break;
+    case 'encryptedChunk':
+      outBuffers.push(message.data.encryptedChunk);
+      updateProgress(message.data.progress);
+      worker.postMessage({ command: 'encryptChunk' }); // next chunk
+      break;
+    case 'finalEncryption':
+      outBuffers.push(message.data.encryptedChunk);
+      updateProgress(message.data.progress);
+      outputFilename = filename + '.cloaker';
+      outFile = new File(outBuffers, outputFilename);
+      downloadLink = document.getElementById('downloadLink');
+      downloadLink.download = filename + '.cloaker';
+      downloadLink.href = URL.createObjectURL(outFile);
+      downloadLink.innerText = `Download encrypted file "${outputFilename}"`
+      downloadLink.style = 'display: unset';
+      break;
+    case 'initializedDecryption':
+      worker.postMessage({ command: 'decryptChunk' }); // kick off decryption
+      break;
+    case 'decryptedChunk':
+      outBuffers.push(message.data.decryptedChunk);
+      updateProgress(message.data.progress);
+      worker.postMessage({ command: 'decryptChunk' });
+      break;
+    case 'finalDecryption':
+      outBuffers.push(message.data.decryptedChunk);
+      updateProgress(message.data.progress);
+      // if filename is longer than .cloaker and ends with .cloaker, chop off extension. if not, leave as is and let the user or OS decide.
+      let suffixes = ['.cloaker', '.cloaker.txt']; // Chrome on Android adds .cloaker.txt for some reason
+      outputFilename = filename;
+      for (i in suffixes) {
+        let len = suffixes[i].length;
+        if (filename.length > len && filename.slice(filename.length - len, filename.length) === suffixes[i]) {
+          outputFilename = filename.slice(0, filename.length - len);
+        }
+      }
+      outFile = new File(outBuffers, outputFilename);
+      downloadLink = document.getElementById('downloadLink');
+      downloadLink.download = outputFilename;
+      downloadLink.href = URL.createObjectURL(outFile);
+      downloadLink.innerText = `Download decrypted file "${outputFilename}"`
+      downloadLink.style = 'display: unset';
+      break;
+    case 'decryptionFailed':
+      output('incorrect password');
+      break;
+    case 'notCloaker':
+      output('file was not encrypted with cloaker');
+      break;
+  }
+};
+
+startEncryption = async (inFile, password) => {
+  inBuffer = await inFile.arrayBuffer();
+  outBuffers = [new Uint8Array(SIGNATURE)];
+  filename = inFile.name;
+  output(`Filename: ${filename}, size: ${inBuffer.byteLength}`);
+  let salt = new Uint8Array(crypto_pwhash_argon2id_SALTBYTES);
+  window.crypto.getRandomValues(salt);
+  outBuffers.push(salt);
+  worker.postMessage({ inBuffer, password, salt, command: 'startEncryption' }, [inBuffer]); // make sure to transfer inBuffer, not clone
+}
+
+startDecryption = async (inFile, password) => {
+  inBuffer = await inFile.arrayBuffer();
+  outBuffers = [];
+  filename = inFile.name;
+  output(`Filename: ${filename}, size: ${inBuffer.byteLength}`);
+  worker.postMessage({ inBuffer, password, filename, command: 'startDecryption' }, [inBuffer]); // make sure to transfer inBuffer, not clone
+}
+
+const output = (msg) => {
+  if (window.getComputedStyle(outputBox).display === 'none') {
+    outputBox.style = 'display: unset';
+  }
+  let message = document.createElement('span');
+  message.textContent = msg;
+  outputBox.appendChild(message);
+  outputBox.appendChild(document.createElement('br'));
+}
+
+const updateProgress = (msg) => {
+  if (window.getComputedStyle(progressBar).display === 'none') {
+    progressBar.style = 'display: unset';
+  }
+  if (!(typeof msg === 'number')) {
+    msg = 100; // if empty file, we divided by 0 in worker, so msg will be NaN
+  }
+  progressBar.value = msg;
+}
--- worker.js
+++ worker.js
@@ -1,32 +1,17 @@
-// import * as c from './constants.js';
-let c = {
-  crypto_pwhash_argon2id_SALTBYTES: 16,
-  CHUNKSIZE: 1024 * 512,
-  LEGACY_CHUNKSIZE: 4096,
-  SIGNATURE: new Uint8Array([0xC1, 0x0A, 0x6B, 0xED]),
-  LEGACY_SIGNATURE: new Uint8Array([0xC1, 0x0A, 0x4B, 0xED]),
-  EXTENSION: '.cloaker',
-  START_ENCRYPTION: 'startEncryption',
-  ENCRYPT_CHUNK: 'encryptChunk',
-  ENCRYPTED_CHUNK: 'encryptedChunk',
-  START_DECRYPTION: 'startDecryption',
-  DECRYPT_CHUNK: 'decryptChunk',
-  DECRYPTED_CHUNK: 'decryptedChunk',
-  INITIALIZED_ENCRYPTION: 'initializedEncryption',
-  INITIALIZED_DECRYPTION: 'initializedDecryption',
-  FINAL_ENCRYPTION: 'finalEncryption',
-  FINAL_DECRYPTION: 'finalDecryption',
-  DECRYPTION_FAILED: 'decryptionFailed',
-};
+const crypto_pwhash_argon2id_SALTBYTES = 16;
+const CHUNKSIZE = 1024 * 512;
+const LEGACY_CHUNKSIZE = 4096;
+const SIGNATURE = new Uint8Array([0xC1, 0x0A, 0x6B, 0xED]);
+const LEGACY_SIGNATURE = new Uint8Array([0xC1, 0x0A, 0x4B, 0xED]);
 
 const hydrate = (sodium) => {
   console.log('sodium initialized in worker');
-  let state, inFile, offset;
+  let state, inBuffer, offset;
   let legacy = false;
 
   const startEncryption = (message) => {
     let { password, salt } = message.data;
-    inFile = message.data.inFile;
+    inBuffer = new Uint8Array(message.data.inBuffer);
     offset = 0;
 
     let key = sodium.crypto_pwhash(32, password, salt,
@@ -37,109 +22,108 @@ const hydrate = (sodium) => {
     let res = sodium.crypto_secretstream_xchacha20poly1305_init_push(key);
     state = res.state;
     let header = res.header;
-    postMessage({ response: c.INITIALIZED_ENCRYPTION, header });
+    postMessage({ response: 'initializedEncryption', header });
   }
 
-  const encryptChunk = async (message) => {
-    let chunkSize = Math.min(c.CHUNKSIZE, inFile.size - offset);
-    let chunk = await inFile.slice(offset, offset + chunkSize).arrayBuffer();
-    chunk = new Uint8Array(chunk);
+  const encryptChunk = (message) => {
+    let chunkSize = Math.min(CHUNKSIZE, inBuffer.byteLength - offset);
+    let chunk = inBuffer.slice(offset, offset + chunkSize);
     offset += chunkSize;
-    let tag = offset < inFile.size - 1
+    let tag = offset < inBuffer.byteLength - 1
       ? sodium.crypto_secretstream_xchacha20poly1305_TAG_MESSAGE
       : sodium.crypto_secretstream_xchacha20poly1305_TAG_FINAL;
     const response = tag === sodium.crypto_secretstream_xchacha20poly1305_TAG_FINAL
-      ? c.FINAL_ENCRYPTION
-      : c.ENCRYPTED_CHUNK;
+      ? 'finalEncryption'
+      : 'encryptedChunk';
     const encryptedChunk = sodium.crypto_secretstream_xchacha20poly1305_push(state, chunk, null, tag);
-    const progress = Math.floor((offset/inFile.size)*100);
-    postMessage({ response, progress, encryptedChunk, bytesWritten: offset });
+    const progress = ((offset/inBuffer.byteLength)*100).toFixed();
+    postMessage({ response, progress, encryptedChunk });
   }
 
-  const startDecryption = async (message) => {
+  const startDecryption = (message) => {
     let { password } = message.data;
-    inFile = message.data.inFile;
+    inBuffer = new Uint8Array(message.data.inBuffer);
     let salt, header, key;
-    let firstFour = await inFile.slice(0, 4).arrayBuffer();
-    firstFour = new Uint8Array(firstFour);
-    if (compareArrays(firstFour, c.SIGNATURE)) {
+    if (compareArrays(inBuffer.slice(0, 4), SIGNATURE)) {
       offset = 4; // skip signature
-      salt = await inFile.slice(offset, offset + c.crypto_pwhash_argon2id_SALTBYTES).arrayBuffer();
-      salt = new Uint8Array(salt);
-      offset += c.crypto_pwhash_argon2id_SALTBYTES;
-      header = await inFile.slice(offset, offset + sodium.crypto_secretstream_xchacha20poly1305_HEADERBYTES).arrayBuffer();
-      header = new Uint8Array(header);
+      salt = new Uint8Array(crypto_pwhash_argon2id_SALTBYTES);
+      salt.set(inBuffer.slice(offset, offset + crypto_pwhash_argon2id_SALTBYTES));
+      offset += crypto_pwhash_argon2id_SALTBYTES;
+      header = inBuffer.slice(offset, offset + sodium.crypto_secretstream_xchacha20poly1305_HEADERBYTES);
       offset += sodium.crypto_secretstream_xchacha20poly1305_HEADERBYTES;
       key = sodium.crypto_pwhash(32, password, salt,
         sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
         sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE,
         sodium.crypto_pwhash_ALG_ARGON2ID13
       );
-    } else {
+    } else if (compareArrays(inBuffer.slice(0, 4), LEGACY_SIGNATURE) || extensionIsCloaker(message.data.filename)) {
       legacy = true;
-      offset = compareArrays(firstFour, c.LEGACY_SIGNATURE) ? 4 : 0; // skip signature
-      salt = await inFile.slice(offset, offset + sodium.crypto_pwhash_scryptsalsa208sha256_SALTBYTES).arrayBuffer();
-      salt = new Uint8Array(salt);
+      offset = compareArrays(inBuffer.slice(0, 4), LEGACY_SIGNATURE) ? 4 : 0; // skip signature
+      salt = new Uint8Array(sodium.crypto_pwhash_scryptsalsa208sha256_SALTBYTES);
+      salt.set(inBuffer.slice(offset, offset + sodium.crypto_pwhash_scryptsalsa208sha256_SALTBYTES));
       offset += sodium.crypto_pwhash_scryptsalsa208sha256_SALTBYTES;
-      header = await inFile.slice(offset, offset + sodium.crypto_secretstream_xchacha20poly1305_HEADERBYTES).arrayBuffer();
-      header = new Uint8Array(header);
+      header = inBuffer.slice(offset, offset + sodium.crypto_secretstream_xchacha20poly1305_HEADERBYTES);
       offset += sodium.crypto_secretstream_xchacha20poly1305_HEADERBYTES;
       key = sodium.crypto_pwhash_scryptsalsa208sha256(32, password, salt,
         sodium.crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE, sodium.crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_INTERACTIVE);
+    } else {
+      postMessage({ response: 'notCloaker' });
+      return;
     }
 
     state = sodium.crypto_secretstream_xchacha20poly1305_init_pull(header, key);
-    postMessage({ response: c.INITIALIZED_DECRYPTION, header });
+    postMessage({ response: 'initializedDecryption', header });
   }
 
-  const decryptChunk = async (message) => {
-    let chunkSize = legacy ? c.LEGACY_CHUNKSIZE : c.CHUNKSIZE;
-    chunkSize = Math.min(chunkSize + sodium.crypto_secretstream_xchacha20poly1305_ABYTES, inFile.size - offset);
-    let chunk = await inFile.slice(offset, offset + chunkSize).arrayBuffer();
-    chunk = new Uint8Array(chunk);
+  const decryptChunk = (message) => {
+    let chunkSize = legacy ? LEGACY_CHUNKSIZE : CHUNKSIZE;
+    chunkSize = Math.min(chunkSize + sodium.crypto_secretstream_xchacha20poly1305_ABYTES, inBuffer.byteLength - offset);
+    let chunk = inBuffer.slice(offset, offset + chunkSize);
     offset += chunkSize;
     let res = sodium.crypto_secretstream_xchacha20poly1305_pull(state, chunk);
     if (!res) {
-      postMessage({ response: c.DECRYPTION_FAILED });
+      postMessage({ response: 'decryptionFailed' });
       return;
     }
     let decryptedChunk = res.message;
-    const progress = Math.floor((offset/inFile.size)*100);
+    const progress = ((offset/inBuffer.byteLength)*100).toFixed();
     let response = res.tag === sodium.crypto_secretstream_xchacha20poly1305_TAG_FINAL
-      ? c.FINAL_DECRYPTION
-      : c.DECRYPTED_CHUNK;
-    postMessage({ response, progress, decryptedChunk, bytesWritten: offset });
+      ? 'finalDecryption'
+      : 'decryptedChunk';
+    postMessage({ response, progress, decryptedChunk });
   }
 
   onmessage = (message) => {
     // console.log('worker received:', message);
     switch(message.data.command) {
-      case c.START_ENCRYPTION:
+      case 'startEncryption':
         startEncryption(message);
         break;
-      case c.ENCRYPT_CHUNK:
+      case 'encryptChunk':
         encryptChunk(message);
         break;
-      case c.START_DECRYPTION:
+      case 'startDecryption':
         startDecryption(message);
         break;
-      case c.DECRYPT_CHUNK:
+      case 'decryptChunk':
         decryptChunk(message);
         break;
     }
   };
 }
 self.sodium = { onload: hydrate };
-importScripts('./sodium.js');
+importScripts('sodium.js');
 
 const compareArrays = (a1, a2) => {
-  if (!a1.length || a1.length != a2.length) {
-    return false;
-  }
   for (let i = 0; i < a1.length; i++) {
     if (a1[i] != a2[i]) {
       return false;
     }
   }
   return true;
+}
+
+const extensionIsCloaker = (filename) => {
+  return filename.length > '.cloaker'.length
+    && filename.slice(filename.length - '.cloaker'.length, filename.length) === '.cloaker'
 }
